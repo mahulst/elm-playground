82a84ee192a0c1544befac62269799fcbfab4f6e
Michel van der Hulst <mvanderhulst@xebia.com>
2016-03-18 11:55:42 +0100

    random gif

---
+++ Main.elm
@@ -0,0 +1,19 @@
+import Effects exposing (Never)
+import RandomGif exposing (init, update, view)
+import StartApp
+import Task
+
+app =
+  StartApp.start
+    { init = init "funny cats 2"
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+main =
+  app.html
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
